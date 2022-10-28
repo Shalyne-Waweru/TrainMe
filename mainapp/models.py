60dcd56fea1e86568f49ed5ae1d4098d01639659
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-10-28 21:27:20 +0300

    created models for reviews, posts and dogs.

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,34 @@
+from tkinter import CASCADE
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
+from accounts.models import Owner, User, gender
+
 # Create your models here.
+class Dog(models.Model):
+    dog_owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='dog')
+    dog_name = models.CharField(max_length=50)
+    dog_age = models.IntegerField()
+    dog_pic = CloudinaryField('image')
+    dog_sex = models.CharField(max_length=10,choices=gender)
+    
+    def __str__(self):
+        return self.dog_name
+        
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    
+    def __str__(self):
+        return self.reviewer + ' ' + self.title
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    video = CloudinaryField(resource_type='video', blank=True)
+    user= models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    
+    def __str__(self):
+        return self.user.username + ' ' + self.title
