586ee81b0e5b015dae76982f8911f5f7f7245516
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-10-28 11:46:15 +0300

    update accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,41 +1,58 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.db import transaction
 
-from .models import User
-    
+from .models import Owner, Trainer, User, places
+from cloudinary.forms import CloudinaryFileField
+
+# Create your forms here.
 class OwnerForm(UserCreationForm):
+    location = forms.ChoiceField(choices=places)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
     password2 = forms.CharField(label='Password Confirm', widget=forms.PasswordInput, required=True)
     class Meta:
         model = User
-        fields =('username','email','phone')
+        fields =('username','first_name','last_name','email','phone','location')
         extra_kwargs = {'password1':{'write_only':True,'min_length':6}}
 
     def create(self,validated_data):
         return User.objects.create_user(**validated_data)
     
-    @transaction.atomic
-    def save(self):
+    def save(self, commit=True):
         user = super().save(commit=False)
-        user.is_Owner = True
-        user.save()
+        user.is_owner = True
+        if commit:
+            user.save()
+        owner = Owner.objects.get(user=user)
+        owner.location= self.cleaned_data.get('location')
+        owner.save()
         return user
-    
+   
 class TrainerForm(UserCreationForm):
+    location = forms.ChoiceField(choices=places)
+    image= CloudinaryFileField()
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
     password2 = forms.CharField(label='Password Confirm', widget=forms.PasswordInput, required=True)
     class Meta:
         model = User
-        fields =('username','email','phone')
+        fields =('username','first_name','last_name','email','phone','image','location')
         extra_kwargs = {'password1':{'write_only':True,'min_length':6}}
 
     def create(self,validated_data):
         return User.objects.create_user(**validated_data)
     
-    @transaction.atomic
-    def save(self):
+    def save(self, commit=True):
         user = super().save(commit=False)
-        user.is_Trainer = True
-        user.save()
+        user.is_trainer = True
+        if commit:
+            user.save()
+        trainer = Trainer.objects.get(user=user)
+        trainer.location= self.cleaned_data.get('location')
+        trainer.image= self.cleaned_data.get('image')
+        trainer.save()
         return user
+    
+        
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password=forms.CharField(max_length=20, widget=forms.PasswordInput)
+   
