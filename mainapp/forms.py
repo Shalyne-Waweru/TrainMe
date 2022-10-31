650ef4fa0b14fd9abd5c9e4e888d313b4073e2c7
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-10-31 22:54:56 +0300

    book and hour forms.

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
-from mainapp.models import Dog, Post, Review
-
+from mainapp.models import Booking, BusinessHours, Clinic, Dog, Post, Review
 # Create your forms here.
 class DogForm(forms.ModelForm):
     class Meta:
@@ -17,3 +16,26 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model= Review
         fields=['title', 'description']
+
+class ClinicForm(forms.ModelForm):
+    class Meta:
+        model= Clinic
+        fields=['clinic_name', 'clinic_location']
+
+class HoursForm(forms.ModelForm):
+    class Meta:
+        model= BusinessHours
+        fields=['day', 'start','end','open_closed']
+        widgets = {
+            'start': forms.TimeInput(attrs={'type': 'time'}),
+            'end': forms.TimeInput(attrs={'type': 'time'})
+        }
+        
+class BookForm(forms.ModelForm):
+    class Meta:
+        model= Booking
+        fields=['email','date', 'time']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'})
+        }
